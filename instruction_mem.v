18f22f6dea19d9eb237a024ff691ea8af2fc5fda
Casalbe <83553693+Casalbe@users.noreply.github.com>
2023-09-03 11:29:11 -0300

    Inicializar a memoria de instrucao com arquivo externo

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,21 +1,16 @@
-module instruction_mem #(
-parameter mem_size = 32
-
-)
+module instruction_mem
 (
   input wire [31:0] address,
   output reg [31:0] data_out
 );
 
-  reg [31:0] memory [0:mem_size - 1];
-// deve-ser feito um jeito de ler um arquivo instructions.list para preencher o array memory
+  parameter mem_size = 32;
+  reg [31:0] mem [0:mem_size-1];
+
   initial begin
-    memory[0] <= 32'b00100000001000000000000000000000;
-    // Mais instrucoes aqui
+    $readmemh ("instruction.list", mem);
   end
 
-  always @(*) begin
-    data_out = memory[address];
-  end
+  assign data_out = mem[address];
 
 endmodule
